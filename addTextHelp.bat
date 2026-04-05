91dbd0cd00b80468961300e8216872490b8b4177
Javier928 <132563587+javier928@users.noreply.github.com>
2026-04-05 20:58:44 +0100

    Add files via upload

---
+++ addTextHelp.bat
@@ -0,0 +1,21 @@
+
+@echo off
+cls
+echo.
+echo  How to use the tool addText.exe:
+echo.
+echo  Add a text to any .png or .jpeg picture using this command
+echo  for the console written in C#.
+echo. 
+echo  Just download the file addText.exe into any folder and
+echo  execute the file.
+echo.
+echo  addText.exe admits two parameters: the .png file and the
+echo  text you want to add to the picture.
+echo.
+echo  Example: addText.exe background.png "javier928"
+echo.
+echo  Result: The text "javier928" will be placed in the picture.
+echo  Find the output.png file in your folder.
+echo.
+pause
